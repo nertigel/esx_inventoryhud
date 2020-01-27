5a538624730e7bcbe67ba2f8a089484c690efe27
Patrik Nertigel <nertigel14475@gmail.com>
2020-01-27 08:59:47 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -116,6 +116,11 @@ function openInventory()
     SetNuiFocus(true, true)
 end
 
+RegisterNetEvent("esx_inventoryhud:doClose")
+AddEventHandler("esx_inventoryhud:doClose", function()
+    closeInventory()
+end)
+
 function closeInventory()
     isInInventory = false
     SendNUIMessage(
